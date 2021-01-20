f68ef8f30eca6decbf8d66cb98684272e3e82135
Qui Le <qui.le@katalon.com>
2021-01-20 17:19:48 +0700

    update

--- create-topic.sh
+++ create-topic.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-./bin/kafka-topics.sh --create --bootstrap-server localhost:9094 --command-config ./config/ssl-user-config.properties --replication-factor 1 --partitions 1 --topic testops-queue
-./bin/kafka-topics.sh --create --bootstrap-server localhost:9094 --command-config ./config/ssl-user-config.properties --replication-factor 1 --partitions 1 --topic testops-admin-queue
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9094 --command-config ./config/ssl-user-config.properties --replication-factor 1 --partitions 3 --topic testops-queue
+./bin/kafka-topics.sh --create --bootstrap-server localhost:9094 --command-config ./config/ssl-user-config.properties --replication-factor 1 --partitions 3 --topic testops-admin-queue
